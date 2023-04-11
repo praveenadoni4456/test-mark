b865c79b7cb3b325f7a13d17a75e559237275476
Praveen Kumar A <55251825+praveenadoni4456@users.noreply.github.com>
2023-04-11 10:51:38 +0530

    Update 01-administration.tf

--- 00-tfc-workspaces/01-administration.tf
+++ 00-tfc-workspaces/01-administration.tf
@@ -14,28 +14,23 @@ resource "tfe_workspace" "cloud-administration-global" {
     oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
   }
 }
-
 resource "tfe_workspace_variable_set" "cloud-administration-global-gcp-org-data" {
   variable_set_id = tfe_variable_set.gcp-org-data.id
   workspace_id    = tfe_workspace.cloud-administration-global.id
 }
-
 resource "tfe_workspace_variable_set" "cloud-administration-global-wif" {
   variable_set_id = tfe_variable_set.workload-identity.id
   workspace_id    = tfe_workspace.cloud-administration-global.id
 }
-
 data "tfe_organization" "tfc-org" {
   name = var.tfc_organization
 }
-
 resource "tfe_variable" "cloud-administration-global-tfc-organization-id" {
   category     = "terraform"
   key          = "tfc_organization_id"
   value        = data.tfe_organization.tfc-org.id
   workspace_id = tfe_workspace.cloud-administration-global.id
 }
-
 resource "tfe_variable" "cloud-administration-global-use-google-oath_token" {
   category     = "terraform"
   key          = "use_google_oath_token"
